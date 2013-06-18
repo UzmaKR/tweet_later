c3bfb342aa59e8c3788fd18a491c6811967d8d6e
Apprentice <apprentice@dbc01.local>
2013-06-18 16:20:12 -0700

    twitter_oauth

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -16,10 +16,16 @@ get '/auth' do
   # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
+
   session.delete(:request_token)
 
   # at this point in the code is where you'll need to create your user account and store the access token
-
+  @user = User.find_by_oauth_token_and_oauth_secret(@access_token.params[:oauth_token], @access_token.params[:oauth_secret] )
+  unless @user
+    @user = User.create(username: @access_token.params[:screen_name],
+                        oauth_token: @access_token.params[:oauth_token],
+                        oauth_secret: @access_token.params[:oauth_secret])
+  end
   erb :index
   
 end
